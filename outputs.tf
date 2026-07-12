a34be757e89522e8c6e6c4ceb1a0b07e3c065b4f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "monitor_alert_processing_rule_action_groups" {
-  description = "All monitor_alert_processing_rule_action_group resources"
-  value       = azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups
-}
 output "monitor_alert_processing_rule_action_groups_add_action_group_ids" {
-  description = "List of add_action_group_ids values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.add_action_group_ids]
+  description = "Map of add_action_group_ids values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.add_action_group_ids }
 }
 output "monitor_alert_processing_rule_action_groups_condition" {
-  description = "List of condition values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.condition]
+  description = "Map of condition values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.condition }
 }
 output "monitor_alert_processing_rule_action_groups_description" {
-  description = "List of description values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.description]
+  description = "Map of description values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.description }
 }
 output "monitor_alert_processing_rule_action_groups_enabled" {
-  description = "List of enabled values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.enabled]
+  description = "Map of enabled values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.enabled }
 }
 output "monitor_alert_processing_rule_action_groups_name" {
-  description = "List of name values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.name]
+  description = "Map of name values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.name }
 }
 output "monitor_alert_processing_rule_action_groups_resource_group_name" {
-  description = "List of resource_group_name values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.resource_group_name }
 }
 output "monitor_alert_processing_rule_action_groups_schedule" {
-  description = "List of schedule values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.schedule]
+  description = "Map of schedule values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.schedule }
 }
 output "monitor_alert_processing_rule_action_groups_scopes" {
-  description = "List of scopes values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.scopes]
+  description = "Map of scopes values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.scopes }
 }
 output "monitor_alert_processing_rule_action_groups_tags" {
-  description = "List of tags values across all monitor_alert_processing_rule_action_groups"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : v.tags]
+  description = "Map of tags values across all monitor_alert_processing_rule_action_groups, keyed the same as var.monitor_alert_processing_rule_action_groups"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_action_group.monitor_alert_processing_rule_action_groups : k => v.tags }
 }
 
